6ef4ccbe62c18a5a083a982831342f4594d70df1
Hell05 <32389641+Bonlin0@users.noreply.github.com>
2019-03-01 18:13:54 +0800

    AZgeek-caculator updated
    gcd in detail

--- AZgeek_md/docs/calculator.md
+++ AZgeek_md/docs/calculator.md
@@ -1,7 +1,6 @@
 # 信息安全数学基础工具
 
-一些计算器工具
-
+## 最大公因数和最小公倍数
 
 <form>
 第一个数字:<br>
@@ -11,7 +10,9 @@
 <input type="text" name="number02" value="" id="number2">
 <br><br>
 </form> 
-<button onclick="calculator_gcd_main()">点击计算最大公因数</button>
-
+<button onclick="calculator_gcd_detail_main()">点击计算最大公因数</button>
 <br/>
+<button onclick="calculator_lcm_main()">点击计算最小公倍数</button>
+
 <p id="show_gcd"></p>
+<p id="show_lcm"></p>
--- AZgeek_md/docs/js/calculator.js
+++ AZgeek_md/docs/js/calculator.js
@@ -2,10 +2,10 @@
  *
  */
 
-function calculator_gcd(number1,number2)
+function calculator_gcd(number1, number2)
 {
-    var a = Math.max(number1,number2);
-    var b = Math.min(number1,number2);
+    var a = Math.max(number1, number2);
+    var b = Math.min(number1, number2);
     var c = a % b;
     while(c!=0)
     {
@@ -19,10 +19,42 @@ function calculator_gcd(number1,number2)
 function calculator_gcd_main()
 {
     var number1 = document.getElementById("number1").value;
-    //alert(number1);
     var number2 = document.getElementById("number2").value;
-    //alert(number2);
     var gcd = calculator_gcd(number1,number2);
-    //alert(gcd);
     document.getElementById("show_gcd").innerHTML="\nThe gcd of "+String(number1)+" and "+String(number2)+" is "+String(gcd)+"\n";
+}
+
+function calculator_lcm_main()
+{
+    var number1 = document.getElementById("number1").value;
+    var number2 = document.getElementById("number2").value;
+    var gcd = calculator_gcd(number1,number2);
+    var lcm = (number1*number2)/gcd;
+    document.getElementById("show_lcm").innerHTML="\nThe lcm of "+String(number1)+" and "+String(number2)+" is "+String(lcm)+"\n";
+}
+
+function calculator_gcd_detail_main()
+{
+    var number1 = document.getElementById("number1").value;
+    var number2 = document.getElementById("number2").value;
+    var process_detail = "";
+    var a = Math.max(number1, number2);
+    var b = Math.min(number1, number2);
+    var q = Math.ceil(a/b);
+    var c = a % b;
+    process_detail = process_detail+ a + " = "+ b +" * " + q;
+    while(c!=0)
+    {
+        process_detail+= " + ";
+        process_detail+= c;
+        process_detail+= "<br/>";
+        a=b;
+        b=c;
+        c=a%b;
+        q=Math.floor(a/b);
+        process_detail = process_detail+ a + " = "+ b +" * " + q;
+    }
+    process_detail+= "<br/>";
+    var gcd = b;
+    document.getElementById("show_gcd").innerHTML="<br/>The gcd of "+String(number1)+" and "+String(number2)+" is "+String(gcd)+"<br/>" + process_detail;
 }
